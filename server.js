0b79b0f38f61e514c7dee5e95084703617ee9299
Oski_pl <oskiplpl@gmail.com>
2018-08-31 10:43:41 +0200

    Fixed hash

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/first-app'));
+app.use(express.static('./dist/first-app'));
 
 app.get('/*', function (req, res) {
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const appRoutes: Routes = [
 @NgModule({
     imports: [
         // RouterModule.forRoot(appRoutes)
-        RouterModule.forRoot(appRoutes, { useHash: true, preloadingStrategy: PreloadAllModules })
+        RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })
     ],
     exports: [
         RouterModule
